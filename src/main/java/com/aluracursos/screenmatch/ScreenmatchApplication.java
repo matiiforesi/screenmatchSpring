27b77134d5d6c1aaae91f61209d42e33f356ffc5
matiiforesi <foresimatias@gmail.com>
2026-01-13 12:11:45 -0300

    nuevo proyecto utilizando Spring Framework

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,14 +1,27 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ScreenmatchApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoApi = new ConsumoAPI();
+        var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=8e33e72f");
+//        var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+        System.out.println(json);
+        ConvierteDatos conversor = new ConvierteDatos();
+        var datos = conversor.obtenerDatos(json, DatosSerie.class);
+        System.out.println(datos);
+    }
 }
